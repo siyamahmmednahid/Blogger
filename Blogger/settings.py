cdf6586dd8e66938bdf3e999b39046d268a8eceb
Siyam Ahmmed Nahid <siyamahmmednahid@Siyams-iMac.local>
2023-03-26 20:48:32 +0600

    Dashboard, Posts and Users app add

--- Blogger/settings.py
+++ Blogger/settings.py
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Blog',
     'Accounts',
+    'Dashboard',
+    'Posts',
+    'Users',
+    'Blog',
     'crispy_forms',
 ]
 
--- Blogger/urls.py
+++ Blogger/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('dashboard/', include('Dashboard.urls')),
+    path('posts/', include('Posts.urls')),
+    path('users/', include('Users.urls')),
     path('blog/', include('Blog.urls')),
     path('accounts/', include('Accounts.urls')),
 ]
